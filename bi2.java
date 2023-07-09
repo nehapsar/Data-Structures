1ad1725b77481df9092bdf449cec6cdfa03b4b88
nehapsar <syedaneha585@gmail.com>
2023-07-09 22:42:17 +0530

    the binary code

---
+++ bi2.java
@@ -0,0 +1,11 @@
+public class bi2{
+public static void biTode(int n){
+int i;
+while(true){
+i=n%2;}
+if( i==0 || i==1){
+System.out.print(i);
+}
+}
+public static void main(String args[]){
+biTode(2);}}
