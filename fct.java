fd4ec08daf3bdc4ca858091c5ad035f5f59a9f08
nehapsar <syedaneha585@gmail.com>
2023-07-12 19:56:18 +0530

    the factorialof a number

---
+++ fct.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class fct{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int p=1;
+
+for(int i=1;i<=n;i++){
+fac=(fac*i);
+}
+System.out.println(p);
+}}
