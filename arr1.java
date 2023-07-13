3616753265a7396918f2fb2d3984b11c066ae2b5
nehapsar <syedaneha585@gmail.com>
2023-07-13 22:32:24 +0530

    create an array

---
+++ arr1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class arr1{
+public static void main(String args[]){
+//int marks[]=new int[19];
+//Scanner sc=new Scanner(System.in);
+//System.out.println("enter your marks");
+//marks[0]=sc.nextInt();
+//marks[1]=sc.nextInt();
+//marks[2]=sc.nextInt();
+int marks[]={12,22,33};
+System.out.print("physics="+marks[0]); }}
