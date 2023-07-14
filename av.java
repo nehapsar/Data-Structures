ade2fe1ecabf9c1722cb7f8a645d93999af0a9dd
nehapsar <syedaneha585@gmail.com>
2023-07-14 10:55:51 +0530

    the avaerage of numbers using inbuilt function in java

---
+++ av.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class av{
+public static void av(int a ,int b, int c){
+int avr=((a+b+c)/3); 
+System.out.print("the avarage of three number is" +avr);
+}
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+
+int a= sc.nextInt();
+int b=sc.nextInt();
+int c=sc.nextInt();
+av(a,b,c);}}
