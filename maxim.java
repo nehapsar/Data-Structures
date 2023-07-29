055a2056dd05f8f4507e102c29a35fa8854e8155
nehapsar <syedaneha585@gmail.com>
2023-07-29 21:01:00 +0530

    the larges number in an array

---
+++ maxim.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class maximum{
+public  static int find(int num[]){
+int largest=Integer.MIN_VALUE;
+for(int i=0;i<num.length;i++){
+    if(largest<num[i]){
+       largest=num[i];}
+}
+return largest;
+
+}
+
+public static void main(String args[]){
+int num[]={1,2,3,4,5,6,7,8,9};
+System.out.println("the maximum element is"+" "+find(num));
+}}
