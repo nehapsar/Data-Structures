2e9289a89ff6620223b37ec6ab5f157b3d5e4c5f
nehapsar <syedaneha585@gmail.com>
2023-08-10 19:34:51 +0530

    findinig of a perenthesis of a stack

---
+++ Stack/prenthesis.java
@@ -0,0 +1,38 @@
+package example;
+import java.util.*;
+public class test {
+
+	public static boolean search(String str) {
+		Stack<Character> s=new Stack<>();
+		for(int i=0;i<str.length();i++) {
+			char ch=str.charAt(i);
+			if(ch=='(' || ch=='{' || ch=='[') {
+				s.push(ch);
+			}
+			else {
+				if(s.isEmpty()) {
+					return false;
+				}
+				if((s.peek()=='(' && ch==')') || 
+						(s.peek()=='[' && ch==']') ||
+						(s.peek()=='{' && ch=='}')) {
+					s.pop();
+				}
+				else {
+					return false;
+				}
+				
+			}
+			
+		}
+		if(s.isEmpty()) {
+			return true;
+		}
+		return false;
+	}
+	public static void main(String[] args) {
+		String str="{{[()]}}]]";
+		System.out.println(search(str));
+	}
+
+}
