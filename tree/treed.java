1c09fdeb1b7450302d8243d75fb89c1cf2ac1ed7
nehapsar <syedaneha585@gmail.com>
2023-08-16 22:03:14 +0530

    ppt of tree

---
+++ tree/treed.java
@@ -0,0 +1,52 @@
+ package treeeeee;
+
+public class treed {
+ static class Node{
+	 int data;
+	 Node left;
+	 Node right;
+	  public Node(int data) {
+		  this.data=data;
+		  this.left=null;
+		  this.right=null;
+			  }
+ }
+	static class BinaryTree{
+		static int idx=-1;
+		public static Node buildtree(int nodes[]) {
+			idx++;
+			if(nodes[idx]==-1) {
+				return null;
+			}
+			Node newNode=new Node(nodes[idx]);
+			newNode.left=buildtree(nodes);
+		newNode.right=buildtree(nodes);
+			return newNode;
+		}
+	
+	public static void postOrder(Node root) {
+		if(root==null) {
+			return;
+		}
+		postOrder(root.left);
+		postOrder(root.right);
+		System.out.print(root.data+" ");
+	}
+	
+	
+	 
+	}
+	
+	
+	
+	public static void main(String[] args) {
+
+		int nodes[]= {1,2,4,7,-1,-1,8,-1,-1,5,-1,-1,3,-1,6,-1,9,-1,-1};
+		//int nodes[]= {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+		BinaryTree tree=new BinaryTree();
+		 Node root =tree.buildtree(nodes);
+		tree.postOrder(root);
+
+	}
+
+}
