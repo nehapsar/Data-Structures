2285134229e3e4041c40af057cb31bc5908a09d8
nehapsar <syedaneha585@gmail.com>
2023-08-17 07:16:09 +0530

    the string functions

---
+++ string/st01.java
@@ -0,0 +1,17 @@
+package string;
+
+public class st7 {
+
+	public static void main(String[] args) {
+		
+		String straa = "ShradhaDidi";
+		String str1 = "ApnaCollege";
+		String str2 = "ShradhaDidi";
+		System.out.println(straa.equals(str1) + " " + straa.equals(str2));
+		
+			String str = "ApnaCollege".replace("l", "xcam");
+			System.out.println(str);
+			}
+			
+
+}
