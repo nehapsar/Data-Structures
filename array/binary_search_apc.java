8d3b309f791545f435c8fa2b1ab692f62190224e
nehapsar <syedaneha585@gmail.com>
2023-08-19 08:44:33 +0530

    the apna college of binary searc

--- array/binary_search_apc.java
+++ array/binary_search_apc.java
@@ -10,16 +10,16 @@ public class bin {
     		if(num[mid]==key) {
     			return mid;
     		}
-    //		else if(num[mid]>key) {
+		else if(num[mid]>key) {
     			ei=mid-1;
     		}
-    //		else {
+		else {
     			si=mid+1;
     		}
     	  }
     	  return -1;
       }
-//	public static void main(String[] args) {
+	public static void main(String[] args) {
 		int num[]= {1,2,3,4,5,6,7,8,9};
 		int key=9;
 		System.out.println(binary(num,key));
