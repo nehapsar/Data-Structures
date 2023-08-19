5bfc76d88d2d993a2fe96a27948178f116234d73
nehapsar <syedaneha585@gmail.com>
2023-08-19 16:12:05 +0530

    the serch of subtree

--- tree/subtree.java
+++ tree/subtree.java
@@ -44,15 +44,15 @@ public class treeb {
 
 
 	public static void main(String[] args) {
-//		Node root=new Node(1);
-//		root.left=new Node(2);
+		Node root=new Node(1);
+		root.left=new Node(2);
 		root.left.left=new Node(4);
 		root.left.right=new Node(5);
 		root.right=new Node(3);
 		root.right.right=new Node(7);
 		root.right.left=new Node(6);
 		
-//		Node subRoot=new Node(2);
+		Node subRoot=new Node(2);
 		subRoot.left=new Node(4);
 		subRoot.right=new Node(5);
 		System.out.println(isSubtree(root,subRoot));
