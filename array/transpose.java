4a13f51eeaa05a3671a47c79575a5c374abdefb3
nehapsar <syedaneha585@gmail.com>
2023-08-20 22:55:21 +0530

    find transpose of two d array final code

--- array/transpose.java
+++ array/transpose.java
@@ -2,7 +2,9 @@ package two_d;
 
 public class td11 {
 	public static void transpose(int matrix[][]) {
-	
+	 
+         //to print the transpose of the matrix
+
 		for(int i=0;i<matrix.length;i++) {
 			for(int j=0;j<matrix[0].length;j++) {
 			System.out.print(matrix[i][j]+" ");
@@ -19,6 +21,8 @@ public static void main(String args[]) {
 	7  8   9
 	10 11  12
 	*/
+
+             //to print the matrix of m*n must past the column and row
 	     int row=4,column=3;
 	     int matrixT[][]=new int[column][row];
 			for(int i=0;i<row;i++) {
