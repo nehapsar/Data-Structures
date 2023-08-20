99d6d5cd6ed2ffc9a7115399191da45213eecfdf
nehapsar <syedaneha585@gmail.com>
2023-08-20 22:52:10 +0530

    find tp of two d array

---
+++ array/transpose.java
@@ -0,0 +1,32 @@
+package two_d;
+
+public class td11 {
+	public static void transpose(int matrix[][]) {
+	
+//		for(int i=0;i<matrix.length;i++) {
+			for(int j=0;j<matrix[0].length;j++) {
+			System.out.print(matrix[i][j]+" ");
+//			}	
+			System.out.println();
+		}
+	}
+	
+public static void main(String args[]) {
+	int matrix[][]= {{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+	/*
+	1  2   3
+	4  5   6
+	7  8   9
+	10 11  12
+	*/
+	     int row=4,column=3;
+	     int matrixT[][]=new int[column][row];
+			for(int i=0;i<row;i++) {
+				for(int j=0;j<column;j++) {
+				matrixT[j][i]=matrix[i][j];
+				}	
+						}
+//			transpose(matrixT);
+}
+}
+
