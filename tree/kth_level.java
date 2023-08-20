d3afea061181a493af6a4bb4d43dc423abd422d2
nehapsar <syedaneha585@gmail.com>
2023-08-20 22:49:00 +0530

    the kth level of tree print the all the elements of kth level final code

--- tree/kth_level.java
+++ tree/kth_level.java
@@ -23,7 +23,7 @@ public class treef {
 		}
 		
 		Kth_level(root.left,level+1,k);
-//		Kth_level(root.right,level+1,k);
+		Kth_level(root.right,level+1,k);
 	}
 	
 	public static void main(String args[]) {
@@ -32,8 +32,8 @@ public class treef {
 	root.left.left=new Node(4);
 	root.left.right=new Node(5);
 	root.right=new Node(3);
-//	root.right.right=new Node(7);
-//	root.right.left=new Node(6);
+	root.right.right=new Node(7);
+	root.right.left=new Node(6);
 	int k =3;
 	Kth_level(root,1,k);
 	}
