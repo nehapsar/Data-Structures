e7678a743e8d3401be1797a7d092692d6486fa7e
nehapsar <syedaneha585@gmail.com>
2023-08-22 22:08:06 +0530

    the fc code pf anagram

--- map/hm5.java
+++ map/hm5.java
@@ -3,19 +3,19 @@ import java.util.*;
 public class hm4 {
 
 public static boolean isAnagram(String s,String t) {
-//	HashMap <Character,Integer> hm=new HashMap<>();
+	HashMap <Character,Integer> hm=new HashMap<>();
 
 		for(int i=0;i<s.length();i++) {
 			char ch=s.charAt(i);
 			hm.put(ch,hm.getOrDefault(ch,0)+1);
 		}
-//		for(int j=0;j<t.length();j++) {
+		for(int j=0;j<t.length();j++) {
 		char ch=t.charAt(j);
 		if(hm.containsKey(ch)) {
 			if(hm.get(ch)==1) {
 				hm.remove(ch);
 			}
-//			else {
+			else {
 				hm.put(ch, hm.get(ch)-1);
 			}
 			
