f2527f3535d5259ac33c46da0c30500bfe04568d
nehapsar <syedaneha585@gmail.com>
2023-08-22 22:12:29 +0530

    the repited element in an array

--- map/hm2.java
+++ map/hm2.java
@@ -2,10 +2,10 @@ package hashmap;
 import java.util.HashMap;
 //import java.util.*;
 public class hm2 {
-//	public static void main(String args[]) {
+	public static void main(String args[]) {
 	int nums[]= {1,3,2,5,1,3,1,5,1};
 	HashMap <Integer , Integer> hm = new HashMap<>() ;
-	//HashMap <String,Integer> hp = new HashMap<>();
+HashMap <String,Integer> hp = new HashMap<>();
 	for(int i=0;i<nums.length;i++) {
 		if(hm.containsKey(nums[i])) {
 //			hm.put(nums[i], hm.get(nums[i])+1);
