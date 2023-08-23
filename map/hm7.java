9e8093a6e5d912db1804d77111c3f2428bd42947
nehapsar <syedaneha585@gmail.com>
2023-08-23 22:28:37 +0530

    the ittarator of an array

--- map/hm7.java
+++ map/hm7.java
@@ -5,11 +5,11 @@ public class hm7 {
 	public static void main(String[] args) {
 			int nums[]= {4,3,2,5,6,7,3,4,2,1,1,1,2,3};
 			
-//	HashSet <Integer> hs = new HashSet<>();
+	HashSet <Integer> hs = new HashSet<>();
 
 
 
-//	for(int i=0;i<nums.length;i++) {
+	for(int i=0;i<nums.length;i++) {
 		hs.add(nums[i]);
 //	}
 	Iterator it= hs.iterator();
