5291cf88867a79d5b90c922c9725cc8bdb9f03d0
nehapsar <syedaneha585@gmail.com>
2023-08-23 22:27:55 +0530

    the ittarator method

---
+++ map/hm7.java
@@ -0,0 +1,23 @@
+package hashmap;
+import java.util.HashSet;
+import java.util.*;
+public class hm7 {
+	public static void main(String[] args) {
+			int nums[]= {4,3,2,5,6,7,3,4,2,1,1,1,2,3};
+			
+//	HashSet <Integer> hs = new HashSet<>();
+
+
+
+//	for(int i=0;i<nums.length;i++) {
+		hs.add(nums[i]);
+//	}
+	Iterator it= hs.iterator();
+	while(it.hasNext()) {
+		System.out.print(it.next()+" ");
+	}
+		}
+
+	}
+
+
