dd6d09f8a30d61dac2a2a585e7d38f79655867da
nehapsar <syedaneha585@gmail.com>
2023-08-25 12:39:52 +0530

    lab program 6

--- lab.java
+++ lab.java
@@ -4,9 +4,9 @@ public class xyz {
 	
 	    public static void main(String[] args)
 	      {
-//	            Shop c = new Shop();
-//	            Producer p1 = new Producer(c, 1);
-//	            Consumer c1 = new Consumer(c, 1);
+	            Shop c = new Shop();
+	            Producer p1 = new Producer(c, 1);
+	            Consumer c1 = new Consumer(c, 1);
 //	            p1.start();
 //	            c1.start();
 	      }
