ceb1d974baa87f38c8887a1ac04f1e2c7a8114e3
nehapsar <syedaneha585@gmail.com>
2023-08-25 12:39:19 +0530

    lab program 6

--- lab.java
+++ lab.java
@@ -1,31 +1,98 @@
-interface Vehicle{
-void drive();
-}
+package lab;
 
+public class xyz {
+	
+	    public static void main(String[] args)
+	      {
+//	            Shop c = new Shop();
+//	            Producer p1 = new Producer(c, 1);
+//	            Consumer c1 = new Consumer(c, 1);
+//	            p1.start();
+//	            c1.start();
+	      }
+	}
+//	class Shop
+	{
+//	      private int materials;
+//	      private boolean available = false;
+//	      public synchronized int get()
+	      {
+	            while (available == false)
+	            {
+	                  try
+	                  {
+	                        wait();
+	                  }
+	                  catch (InterruptedException ie)
+	                  {
+	                  }
+	            }
+	            available = false;
+	            notifyAll();
+	            return materials;
+	      }
+	      public synchronized void put(int value)
+	      {
+	            while (available == true)
+	            {
+	                  try
+	                  {
+	                        wait();
+	                  }
+	                  catch (InterruptedException ie)
+	                  {
+	                        ie.printStackTrace();
+	                  }
+	            }
+	            materials = value;
+	            available = true;
+	            notifyAll();
+	      }
+	}
+	class Consumer extends Thread
+	{
+	      private Shop Shop;
+	      private int number;
+	      public Consumer(Shop c, int number)
+	      {
+	            Shop = c;
+	            this.number = number;
+	      }
+	      public void run()
+	      {
+	            int value = 0;
+	            for (int i = 0; i < 10; i++)
+	            {
+	                  value = Shop.get();
+	                  System.out.println("Consumed value " + this.number+ " got: " + value);
+	            }
+	      }
+	}
+	class Producer extends Thread
+	{
+	      private Shop Shop;
+	      private int number;
 
+	      public Producer(Shop c, int number)
+	      {
+	            Shop = c;
+	            this.number = number;
+	      }
+	      public void run()
+	      {
+	            for (int i = 0; i < 10; i++)
+	            {
+	                  Shop.put(i);
+	                  System.out.println("Produced value " + this.number+ " put: " + i);
+	                  try
+	                  {
+	                        sleep((int)(Math.random() * 100));
+	                  }
+	                  catch (InterruptedException ie)
+	                  {
+	                        ie.printStackTrace();
+	                  }
+	            }
+	      }
+	}
 
-abstract class Abstractvehicle implements Vehicle{
-public abstract void drive();}
-
-
-
-class Car extends abstractvehicle{
-@Override
-public void drive(){
-System.out.println("the car is ridding");}}
-
-
-
-class motorcycle extends abstractvehicle{
-@Override
-public void  drive(){
-System.out.println("the motor cycle is riding");}}
-
-
-
-
-
-public class main{
-public static void main(STring args[]){
-Vehicle v1=new Car();
-v1.drive();}}
