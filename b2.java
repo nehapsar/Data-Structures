9da292dc28e59bc1f3299daa98eb7aa374f2c0b4
nehapsar <syedaneha585@gmail.com>
2023-08-26 19:34:19 +0530

    the even or odd number..

---
+++ b2.java
@@ -0,0 +1,18 @@
+package bit;
+
+public class b2 {
+public static int ith_bit(int n,int i) {
+	int Bitmask=1;
+	if((  n & Bitmask<<i) == 0) {
+		return 0;
+	}
+	return 1;
+}
+//	public static void main(String[] args) {
+		int n=8;
+		int i=2;
+//		System.out.println(ith_bit(n,i));
+//
+	}
+
+}
