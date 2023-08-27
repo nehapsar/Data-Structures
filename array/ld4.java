c1fd6aa75f6c1239ba2f6e7194cf039edcbf1793
nehapsar <syedaneha585@gmail.com>
2023-08-27 23:12:45 +0530

    the ld problem of search element and insert at perticular index

--- array/ld4.java
+++ array/ld4.java
@@ -9,7 +9,7 @@ public class ld4 {
 	        if(nums[i]==target){
 	            return i;
 	        }
-//	        else if(nums[i]<target && nums[i+1]>target  && i<n){
+	        else if(nums[i]<target && nums[i+1]>target  && i<n){
 	           return i+1;
 	        }
 	    }       
@@ -20,7 +20,7 @@ public class ld4 {
 	    public static void main(String args[]) {
 	    	int nums[]= {1,3,5,6};
 	    	int target = 7;
-//	    	System.out.println(searchInsert(nums,target));
+	    	System.out.println(searchInsert(nums,target));
 	    }
 }
 
