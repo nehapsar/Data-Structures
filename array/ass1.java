3f801ae8b432abd15f2f966f3160029af0eee49e
nehapsar <syedaneha585@gmail.com>
2023-08-27 23:17:12 +0530

    the apc pb stage 3

--- array/ass1.java
+++ array/ass1.java
@@ -38,7 +38,7 @@ public class x {
 int left = 0;
 		int right = nums.length-1;
 }
-	//	while(left < right){
+	while(left < right){
 			int mid = left + (right - left)/2;
 			}
 			return mid;
@@ -51,10 +51,10 @@ int left = 0;
 			right = mid-1;
 			return left;
 			}
-	//	public static void main(String args[]) {
-	//		int nums[] = {4, 5, 6, 7, 0, 1, 2};
+	public static void main(String args[]) {
+		int nums[] = {4, 5, 6, 7, 0, 1, 2};
 			int target = 0;
-	//		System.out.println(search(nums ,target));
+		System.out.println(search(nums ,target));
 		}
 		}
 		
