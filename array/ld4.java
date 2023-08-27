389b3009da19ae2b36794d8c1f04e89fa8742af3
nehapsar <syedaneha585@gmail.com>
2023-08-27 23:10:45 +0530

    the ld problem

---
+++ array/ld4.java
@@ -0,0 +1,26 @@
+package array;
+
+public class ld4 {
+
+	
+	    public static int searchInsert(int[] nums, int target) {
+	        int n=nums.length -1;
+//	    for(int i=0;i<n;i++){
+	        if(nums[i]==target){
+	            return i;
+	        }
+//	        else if(nums[i]<target && nums[i+1]>target  && i<n){
+	           return i+1;
+	        }
+	    }       
+	    return n+1; 
+	     
+	    
+//	}
+	    public static void main(String args[]) {
+	    	int nums[]= {1,3,5,6};
+	    	int target = 7;
+//	    	System.out.println(searchInsert(nums,target));
+	    }
+}
+//
