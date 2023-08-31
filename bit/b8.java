0f7b16f5b2a7aa061057d0b5bdbcb907569d0b58
nehapsar <syedaneha585@gmail.com>
2023-08-31 21:52:27 +0530

    the pow of num

---
+++ bit/b8.java
@@ -0,0 +1,14 @@
+public class b8 {
+       public static int PowerOfNumber(int a , int n) {
+    	    
+    	   int ans=1;
+    	   
+    	   while(n > 0) {
+    		   if((n & 1) != 0) {
+    			   ans = ans * a;
+    			   }
+    		   a = a * a ;
+    		     n =n>>1;
+    	   }
+    	   return ans;
+       }
