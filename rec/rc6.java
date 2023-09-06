3b7baeb43aa26ea6c59f9e13a4d002668901e528
nehapsar <syedaneha585@gmail.com>
2023-09-06 11:30:27 +0530

    find the first occurence of an element

--- rec/rc6.java
+++ rec/rc6.java
@@ -9,3 +9,13 @@ public static int search(int num[] ,int key,int i) {
 		 return i;
 	}
 	
+
+	return search(num , key,i+1);
+}
+	
+	public static void main(String[] args) {
+int num[]= {1,2,3,4,4,5,6,7,8};int key=4;
+System.out.println(search(num,key,0));
+	}
+
+}
