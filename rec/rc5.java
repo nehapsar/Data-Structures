847b127a85286d7846a3844e413b4522c36e5bd6
nehapsar <syedaneha585@gmail.com>
2023-09-06 11:25:30 +0530

    the array sorted or notfinal apc

--- rec/rc5.java
+++ rec/rc5.java
@@ -10,3 +10,13 @@ public class rc5 {
 		if(num[i+1] <num[i]) {
 			return false;
 		}
+
+		return isSorted(num,i+1);
+	}
+
+	public static void main(String[] args) {
+      int num[]= {1,2,3,4,5,9,6};
+      System.out.println(isSorted(num,0));
+	}
+
+}
