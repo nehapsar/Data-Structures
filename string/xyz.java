7201f33049c9f973577fc8a956ff4c5035b88dc6
nehapsar <syedaneha585@gmail.com>
2023-09-08 12:02:57 +0530

    last char length

---
+++ string/xyz.java
@@ -0,0 +1,13 @@
+public class xyz {
+	
+	    public static  int lengthOfLastWord(String s) {
+	        StringBuilder sb =new StringBuilder(" ");
+	        s =s.trim();
+	        for(int i=s.length()-1;i>=0;i--){
+	           if(s.charAt(i)==' '){
+	             return sb.length()-1;
+	           }
+	           else {
+	           sb.append(s.charAt(i));
+	           
+	        
