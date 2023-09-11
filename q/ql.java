8207248535ad8b4a071fd8b07698670cce8d6c4c
nehapsar <syedaneha585@gmail.com>
2023-09-11 21:18:50 +0530

    the bs code

---
+++ q/ql.java
@@ -0,0 +1,14 @@
+package q;
+
+public class ql {
+	static class Node{
+		int data;
+		Node next;
+		Node(int data){
+			this.data=data;
+			this.next=null;
+		}}
+		static class qll{
+			static Node head;
+			static Node tail ;
+			
