f908801340aab98c52653d436a76ecdc24b5b1c2
nehapsar <syedaneha585@gmail.com>
2023-09-12 20:33:22 +0530

    the q usin 2 stack

---
+++ q/qs.java
@@ -0,0 +1,11 @@
+package q;
+import java.util.*;
+public class qs {
+	static class queue{
+		static Stack<Integer> s1=new Stack<>();
+		static Stack<Integer> s2=new Stack<>();
+		//to check the stack is empty
+		public static boolean isEmpty() {
+			return s1.isEmpty();
+		}
+		// to add
