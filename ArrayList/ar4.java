f81f046310268ed6a17397ae3878cdb4aba50ac0
nehapsar <syedaneha585@gmail.com>
2023-09-16 10:05:39 +0530

    minimum of the arrray

--- ArrayList/ar4.java
+++ ArrayList/ar4.java
@@ -12,3 +12,16 @@ public class ar4 {
 		}
 		return mx;
 	}
+
+			public static void main(String[] args) {
+				ArrayList<Integer> list =new ArrayList<>();
+				list.add(1);
+				list.add(2);
+				list.add(3);
+				list.add(4);
+				list.add(5);
+				list.add(6);
+	System.out.println(minimum(list));
+		}
+
+	}
