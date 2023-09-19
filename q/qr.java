d72771ff208539f10a07dae561d6a691dd69687b
nehapsar <syedaneha585@gmail.com>
2023-09-19 19:40:38 +0530

    the reverse of a q

--- q/qr.java
+++ q/qr.java
@@ -10,3 +10,14 @@ public class qr {
     		q1.add(s1.pop());
     	}
     }
+public static void main(String[] args) {
+		Queue <Integer> q1 = new LinkedList<>();
+		
+		q1.add(1);q1.add(2);q1.add(3);q1.add(4);q1.add(5);
+		reverse(q1);
+		while(!q1.isEmpty()) {
+			System.out.println(q1.remove()+" ");
+		}
+	}
+
+}
