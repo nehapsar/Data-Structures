b9a4de26517053cb686ab35ec06a1fa89a0f68d3
nehapsar <syedaneha585@gmail.com>
2023-09-19 19:37:41 +0530

    the pointer approach

--- ArrayList/ar8.java
+++ ArrayList/ar8.java
@@ -10,3 +10,19 @@ public static boolean find(ArrayList <Integer> al , int target) {
 			if((al.get(lp) + al.get(rp))<target) {
 				lp++;
 			}
+else {
+				rp--;
+			}
+		 }
+		
+	
+	return false;
+}
+	public static void main(String[] args) {
+		ArrayList<Integer> al = new  ArrayList<>();
+ al.add(1); al.add(2); al.add(3); al.add(4); al.add(5); al.add(6); al.add(7); al.add(8);
+ System.out.print
+ System.out.println(find(al ,5));
+	}
+
+}
