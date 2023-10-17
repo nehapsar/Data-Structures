8a4ad9bad95c8a2446efa368e388210e41f0f516
nehapsar <syedaneha585@gmail.com>
2023-10-17 18:23:26 +0530

    horsepools algorithm

--- horspools.java
+++ horspools.java
@@ -11,3 +11,19 @@ public class HorspoolStringMatch {
             System.out.println("Pattern not found in the text");
         }
     }
+public static int horspoolStringMatch(String text, String pattern) {
+        int textLength = text.length();
+        int patternLength = pattern.length();
+
+        if (patternLength > textLength) {
+            return -1; // Pattern cannot be longer than the text
+        }
+
+        int[] shiftTable = generateShiftTable(pattern);
+
+        int i = patternLength - 1;
+        while (i < textLength) {
+            int k = 0;
+            while (k < patternLength && pattern.charAt(patternLength - 1 - k) == text.charAt(i - k)) {
+                k++;
+            }
